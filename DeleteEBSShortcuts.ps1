cefc7124a60f2ded620bbc0f93738ddb857664f5
0v3rz34l0u5 <37838108+0v3rz34l0u5@users.noreply.github.com>
2020-03-31 17:24:44 +0100

    Add files via upload

---
+++ DeleteEBSShortcuts.ps1
@@ -0,0 +1,22 @@
+﻿$cpu = (Get-WMIObject Win32_Processor).AddressWidth
+if ($cpu -eq 64) {
+        $regSteamKey = "HKLM:\SOFTWARE\WOW6432Node\Valve\Steam" }
+    else {
+        $regSteamKey = "HKLM:\SOFTWARE\Valve\Steam"
+}
+$steamInstallDir = Get-ItemProperty -path "$regSteamKey" -name "InstallPath"
+$steamFolder = $steamInstallDir.InstallPath
+## Gets install location of the Insurgency Sandstorm (the batch file(s) will be created here).
+$insurgencySandstormInstallDir = Get-ItemProperty -path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 581320\' -name "InstallLocation"
+$insurgencySandstormFolder = $insurgencySandstormInstallDir.InstallLocation
+$coopBatchFileName = "ebscoopserver.bat"
+$pvpBatchFileName = "ebspvpserver.bat"
+$startMenuFolder = "Microsoft\Windows\Start Menu\Programs"
+$coopShortcutFilename = "EBS Co-Op Insurgency Sandstorm Server.lnk"
+$coopShortcutFile = "$($env:APPDATA)\$($startMenuFolder)\$($coopShortcutFilename)"
+$pvpShortcutFilename = "EBS PVP Insurgency Sandstorm Server.lnk"
+$pvpShortcutFile = "$($env:APPDATA)\$($startMenuFolder)\$($pvpShortcutFilename)"
+del "$insurgencySandstormFolder\$coopBatchFileName"
+del "$insurgencySandstormFolder\$pvpBatchFileName"
+del "$coopShortcutFile"
+del "$pvpShortcutFile"
