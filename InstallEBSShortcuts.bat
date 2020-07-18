cd8de4eba3641c9f5165ae19c09dd8f0db842c0a
0verzeal0us <37838108+0verzeal0us@users.noreply.github.com>
2020-07-18 11:28:10 +0100

    Update InstallEBSShortcuts.bat

--- InstallEBSShortcuts.bat
+++ InstallEBSShortcuts.bat
@@ -1,6 +1,5 @@
 echo off
 CHCP 65001
-powershell.exe -command "(Invoke-WebRequest https://raw.githubusercontent.com/0v3rz34l0u5/Enemy-Boat-Spotted/Sandstorm/CreateEBSShortcuts.ps1).content | Out-File -FilePath c:\CreateEBSSandstormShortcuts.ps1"
+powershell.exe -command "(Invoke-WebRequest https://raw.githubusercontent.com/0verzeal0us/Enemy-Boat-Spotted/Sandstorm/CreateEBSShortcuts.ps1).content | Out-File -FilePath c:\CreateEBSSandstormShortcuts.ps1"
 powershell.exe –ExecutionPolicy Bypass "c:\CreateEBSSandstormShortcuts.ps1"
 del "c:\CreateEBSSandstormShortcuts.ps1"
-
